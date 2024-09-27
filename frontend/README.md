e57edfae78a97114b4ddc8836a1da4384c95c325
sumeet-srknec <sumeet.srknec@gmail.com>
2024-09-27 08:25:55 +0100

    adding additional boilterplate

--- frontend/README.md
+++ frontend/README.md
@@ -1,2 +1,1 @@
 # Spotify-Alt: Yet another Spotify
-
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
+
+// npm install -D tailwindcss postcss autoprefixer prettier @headlessui/react @reduxjs/toolkit prettier-plugin-tailwindcss react-icons react-redux dotenv react-router-dom
+// npx tailwindcss init
