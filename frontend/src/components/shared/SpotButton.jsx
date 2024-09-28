6084990e66d464b66a56d8dadfdbc6d9b8fa4aa9
sumeet-srknec <sumeet.srknec@gmail.com>
2024-09-28 19:36:10 +0100

    adding control panel

--- frontend/src/components/shared/SpotButton.jsx
+++ frontend/src/components/shared/SpotButton.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 function SpotButton({ children, onClick }) {
     return (
         <div
-            className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-2 sm:pr-0"
+            className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-1 sm:pr-0"
             onClick={onClick}
         >
             <button
                 type="button"
-                className="relative rounded-full bg-gray-800 p-2 text-gray-400 hover:text-white hover:bg-gray-700/50 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                className="relative rounded-full bg-black p-2 text-gray-400 hover:text-white hover:bg-gray-700/50 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
             >
                 <span className="absolute -inset-1.5" />
                 <span className="sr-only">Home</span>
