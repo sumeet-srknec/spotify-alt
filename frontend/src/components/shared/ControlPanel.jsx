451bc06f23515f0f1819bc136faefecfac7efc7d
sumeet-srknec <sumeet.srknec@gmail.com>
2024-10-03 09:18:30 +0100

    removing unused code

--- frontend/src/components/shared/ControlPanel.jsx
+++ frontend/src/components/shared/ControlPanel.jsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import React, { useState } from 'react'
+import React from 'react'
 import { AiOutlinePlaySquare } from 'react-icons/ai'
 import { BsDot } from 'react-icons/bs'
 import { CgMiniPlayer } from 'react-icons/cg'
