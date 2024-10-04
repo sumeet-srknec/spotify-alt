1f511144c21f2e24193fd156ad3d93287903fd5d
sumeet-srknec <sumeet.srknec@gmail.com>
2024-10-04 11:18:40 +0100

    fixed a typo in friend activity dialog

--- frontend/src/components/shared/Header.jsx
+++ frontend/src/components/shared/Header.jsx
@@ -63,14 +63,14 @@ function Header() {
                             </div>
                             <div
                                 className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
-                                onClick={(e) => dispatch(openPanel('Friends Activity'))}
+                                onClick={(e) => dispatch(openPanel('Friend Activity'))}
                             >
                                 <button
                                     type="button"
                                     className="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                                 >
                                     <span className="absolute -inset-1.5" />
-                                    <span className="sr-only">View Notifications</span>
+                                    <span className="sr-only">View Friend Activity</span>
                                     <HiOutlineUserGroup aria-hidden="true" className="h-6 w-6" />
                                 </button>
                             </div>
