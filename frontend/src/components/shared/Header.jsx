868ca05176dab6476fdadb6b49be3a0f5f3dd560
sumeet-srknec <sumeet.srknec@gmail.com>
2024-10-08 09:22:14 +0100

    fixing https://github.com/sumeet-srknec/spotify-alt/issues/17

--- frontend/src/components/shared/Header.jsx
+++ frontend/src/components/shared/Header.jsx
@@ -19,7 +19,7 @@ function Header() {
                             <HiChevronRight className="h-6 w-auto hover:text-white" />
                         </div>
                         {/* Central search bar */}
-                        <div className="flex flex-shrink-0 items-center cursor-pointer">
+                        <div className="flex flex-row items-center cursor-pointer">
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                                 <button
                                     type="button"
@@ -30,7 +30,7 @@ function Header() {
                                     <HiHome aria-hidden="true" className="h-6 w-6" />
                                 </button>
                             </div>
-                            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 ">
+                            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-2 sm:pr-0 ">
                                 <div className="flex flex-row gap-2 items-center relative w-auto rounded-full p-2 bg-gray-800 text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                                     {/* <span className='absolute -inset-1.5' /> */}
                                     <span className="sr-only">Search you songs</span>
